70bb1fa7fea49423b3f1b12f351629e18eab367a
grandmasterspoc <dilloncortez@gmail.com>
2015-12-27 21:31:07 -0700

    Fixed bug that was causeing error

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -6,14 +6,13 @@ module Rulers
   class Application
     def call(env)
       if env['PATH_INFO'] == '/favicon.ico'
-        return [404, {'Content_type' => text/html}, []]
+        return [404, {'Content_type' => 'text/html'}, []]
       end  
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'},
-        [text]]
+      [200, {'Content-Type' => 'text/html'}, [text]]
     end  
   end  
 
